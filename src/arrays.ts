ca2d0e34c7f99386192a77eae53659d7ea563976
Nathanael <nathanpl@udel.edu>
2024-02-20 19:37:03 -0500

    Finished strings to int function

--- src/arrays.ts
+++ src/arrays.ts
@@ -22,7 +22,8 @@ export function tripleNumbers(numbers: number[]): number[] {
  * the number cannot be parsed as an integer, convert it to 0 instead.
  */
 export function stringsToIntegers(numbers: string[]): number[] {
-    return [];
+    const convert = numbers.map((num) => (parseInt(num) ? parseInt(num) : 0));
+    return convert;
 }
 
 /**
