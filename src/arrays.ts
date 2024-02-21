258fb32162ef51720ce2d3080f353815ee1948ba
Nathanael <nathanpl@udel.edu>
2024-02-20 19:31:40 -0500

    Finished triple numbers func

--- src/arrays.ts
+++ src/arrays.ts
@@ -13,7 +13,8 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    const tripleNum = numbers.map((num: number): number => num * 3);
+    return tripleNum;
 }
 
 /**
