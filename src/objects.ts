79f851dc84b99d541d04990d012daaeb32f50988
Nathanael <nathanpl@udel.edu>
2024-02-27 18:31:46 -0500

    finished toMarkdown

--- src/objects.ts
+++ src/objects.ts
@@ -79,6 +79,15 @@ export function toShortForm(question: Question): string {
  * Check the unit tests for more examples of what this looks like!
  */
 export function toMarkdown(question: Question): string {
+    if (question.type === "short_answer_question") {
+        return "# " + question.name + "\n" + question.body;
+    }
+    if (question.type === "multiple_choice_question") {
+        const options = question.options
+            .map((option) => `- ${option}`)
+            .join("\n");
+        return "# " + question.name + "\n" + question.body + "\n" + options;
+    }
     return "";
 }
 
